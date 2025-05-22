ed9fa260ff1829c93e550cc454d9491306f5eb80
MohitD-7 <201695308+MohitD-7@users.noreply.github.com>
2025-05-22 14:17:27 +0530

    Update PHQ - Bulk Specs Converter - Streamlit.py

--- PHQ - Bulk Specs Converter - Streamlit.py
+++ PHQ - Bulk Specs Converter - Streamlit.py
@@ -1030,8 +1030,7 @@ def main():
         # Use columns to place logo to the right
         col1, col2 = st.columns([4,1])
         with col1:
-            st.title("PatioHQ Specs HTML Converter
-            (Tabs & Dropdowns)")
+            st.title("PatioHQ Specs HTML Converter<br>(Tabs & Dropdowns)")
         with col2:
             st.image(logo_path, width=113)
     else:
